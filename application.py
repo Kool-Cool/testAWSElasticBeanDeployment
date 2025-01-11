0124aaaa679b24d1c7918b4a908bed7903126bc3
Kool-Cool <112835534+Kool-Cool@users.noreply.github.com>
2025-01-11 21:38:32 +0530

    Update application.py

--- application.py
+++ application.py
@@ -8,4 +8,4 @@ def home():
     # return render_template('index.html')
 
 if __name__ == '__main__':
-    application.run(debug=False , port=8000)
+    application.run(debug=False , host='0.0.0.0' , port=8000)
