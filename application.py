745cbf56afde82dbff7c615eb514957039945207
Kool-Cool <112835534+Kool-Cool@users.noreply.github.com>
2025-01-11 21:39:07 +0530

    update : render

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ application = Flask(__name__)
 
 @application.route('/')
 def home():
-    return "render_template('index.html')"
-    # return render_template('index.html')
+    # return "render_template('index.html')"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     application.run(debug=False , host='0.0.0.0' , port=8000)
